be47acf5aa405dfec4b1f31d1b68208a1f8d985f
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-10-20 20:09:48 +0900

    fix

--- src/domain/shaper.ts
+++ src/domain/shaper.ts
@@ -4,41 +4,46 @@ export function shaper(text: string) {
   if (!text.split(intro)[2]) return "";
   now = intro + text.split(intro)[2];
 
-  let arr = now
-    .split("\n")
-    .filter(s => s.slice(0, 9) !== "Rosenberg" && s.slice(0, 3) !== "RFC");
+  let arr = now.split("\n").reduce(
+    (acc, cur) => {
+      if (cur.slice(0, 9) === "Rosenberg") {
+        acc = [...acc].slice(0, -3);
+      } else if (
+        acc[acc.length - 3] &&
+        acc[acc.length - 3].slice(0, 3) === "RFC"
+      ) {
+        const next = [...acc].slice(0, -4);
+        next.push(cur);
+        acc = next;
+      } else acc.push(cur);
+      return acc;
+    },
+    [] as string[]
+  );
+
+  arr = arr.map(s => (s.slice(0, 3) === "   " ? s.slice(2, s.length) : s));
 
   console.log([...arr]);
 
   arr = arr.reduce(
     (acc, cur) => {
       const last = acc.slice(-1)[0];
-      if (cur === "" && last !== "" && last !== "\n") acc.push("\n");
+      if (cur === "" && last !== "" && last !== "\n") acc.push("\n\n");
       else if (cur !== "") acc.push(cur);
       return acc;
     },
     [] as string[]
   );
 
-  console.log([...arr]);
+  //   console.log([...arr]);
 
-  now = arr
-    .reduce(
-      (acc, cur) => {
-        const last = acc.slice(-1)[0];
-        if (cur.length > 1) acc.push(cur);
-        else if (cur.length === 1 && last.length > 1) acc.push(cur);
-        return acc;
-      },
-      [] as string[]
-    )
-    .map(s => (s.slice(0, 3) === "   " ? s.slice(2, s.length) : s))
+  //   arr = arr.reduce(
+  //     (acc, cur) => {
+  //       return acc;
+  //     },
+  //     [] as string[]
+  //   );
 
-    .join("")
-    .split("\n")
-    .map(s => (s.slice(0, 1) === " " ? "\n\n" + s.slice(0, s.length) : s))
-    .join("");
-
-  console.log(now.split("\n"));
+  now = arr.join("");
   return now;
 }
