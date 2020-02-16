6dc3a6b3bb4eacc5e93aed820a3e4152146ada0e
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-02-16 17:19:22 +0900

    build

---
+++ build/precache-manifest.29856424bbc7cc831c5bc08ec3434ec5.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d97fd0d0f90f314fc01",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "c92b85a5b907c70211f4ec25e29a8c4a",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "bb6af454e368d0551473b14bb6c21a2a",
+    "url": "index.html"
+  }
+]);
--- src/domain/shaper.ts
+++ src/domain/shaper.ts
@@ -5,36 +5,30 @@ export function shaper(text: string) {
   now = intro + text.split(intro)[2];
 
   // remove page header
-  let arr = now.split("\n").reduce(
-    (acc, cur) => {
-      if (cur.slice(0, 9) === "Rosenberg") {
-        acc = [...acc].slice(0, -3);
-      } else if (
-        acc[acc.length - 3] &&
-        acc[acc.length - 3].slice(0, 3) === "RFC"
-      ) {
-        const next = [...acc].slice(0, -4);
-        next.push(cur);
-        acc = next;
-      } else acc.push(cur);
-      return acc;
-    },
-    [] as string[]
-  );
+  let arr = now.split("\n").reduce((acc, cur) => {
+    if (cur.includes("Standards Track")) {
+      acc = [...acc].slice(0, -3);
+    } else if (
+      acc[acc.length - 3] &&
+      acc[acc.length - 3].slice(0, 3) === "RFC"
+    ) {
+      const next = [...acc].slice(0, -4);
+      next.push(cur);
+      acc = next;
+    } else acc.push(cur);
+    return acc;
+  }, [] as string[]);
 
   // remove unnecessary space
   arr = arr.map(s => s.trim() + (s !== "" ? " " : ""));
 
   // divide sentence block
-  arr = arr.reduce(
-    (acc, cur) => {
-      const last = acc.slice(-1)[0];
-      if (cur === "" && last !== "" && last !== "\n") acc.push("\n\n");
-      else if (cur !== "") acc.push(cur);
-      return acc;
-    },
-    [] as string[]
-  );
+  arr = arr.reduce((acc, cur) => {
+    const last = acc.slice(-1)[0];
+    if (cur === "" && last !== "" && last !== "\n") acc.push("\n\n");
+    else if (cur !== "") acc.push(cur);
+    return acc;
+  }, [] as string[]);
 
   now = arr.join("");
   return now;
