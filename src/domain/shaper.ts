a6fe91869e80501228413143520b97a5491d1bb5
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-02-16 23:10:36 +0900

    a

--- src/domain/shaper.ts
+++ src/domain/shaper.ts
@@ -1,16 +1,15 @@
 export function shaper(text: string) {
   let now = text;
-  const intro = "1.  Introduction";
-  if (!text.split(intro)[2]) return "";
-  now = intro + text.split(intro)[2];
 
   // remove page header
   let arr = now.split("\n").reduce((acc, cur) => {
+    // if (cur.includes("[Page "))
     if (cur.includes("Standards Track")) {
       acc = [...acc].slice(0, -3);
     } else if (
       acc[acc.length - 3] &&
       acc[acc.length - 3].slice(0, 3) === "RFC"
+      // acc[acc.length - 3].slice(0, 3) === "Int"
     ) {
       const next = [...acc].slice(0, -4);
       next.push(cur);
